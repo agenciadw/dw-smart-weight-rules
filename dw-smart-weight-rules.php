c9dc253af3bf12a2dcd7dc2b5efcc4bcbbe77abc
David William da Costa <david@dwdigital.com.br>
2026-02-24 08:53:33 -0300

    Update dw-smart-weight-rules.php

--- dw-smart-weight-rules.php
+++ dw-smart-weight-rules.php
@@ -11,7 +11,7 @@
  * Requires at least: 5.8
  * Requires PHP: 7.4
  * WC requires at least: 6.0
- * WC tested up to: 9.4
+ * WC tested up to: 10.5.2
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  *
